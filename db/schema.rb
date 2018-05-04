4d006062d8eb336fddffee3191289ce4f6dc5119
Boots2291 <buteramv@gmail.com>
2018-05-04 14:41:31 -0600

    Add basic user login and admin account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504192701) do
+ActiveRecord::Schema.define(version: 20180504202726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20180504192701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_path"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
