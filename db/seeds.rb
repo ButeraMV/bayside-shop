dede669ee017d97a4eacae4d369e82f5ec7bf5cb
Boots2291 <buteramv@gmail.com>
2018-05-06 13:10:57 -0600

    Change background

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-Product.create(name: "Bayside Shirt", price: 99.99, img_path: 'BB-shirt.png')
-Product.create(name: "Bayside Koozie", price: 99.99, img_path: 'BB-koozie.png')
+Product.create(name: "Bayside Shirt", price: 19.95, img_path: 'BB-shirt.png')
+Product.create(name: "Bayside Koozie", price: 4.95, img_path: 'BB-koozie.png')
+Product.create(name: "Coming Soon", price: 9.99, img_path: '200.png')
