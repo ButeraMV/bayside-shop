d0e6b8a8d0cb7f33b4781870280850a3a178e00d
Boots2291 <buteramv@gmail.com>
2018-05-07 10:49:26 -0600

    Add items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 Product.create(name: "Bayside Shirt", price: 19.95, img_path: 'BB-shirt.png')
 Product.create(name: "Bayside Koozie", price: 4.95, img_path: 'BB-koozie.png')
-Product.create(name: "Coming Soon", price: 9.99, img_path: '200.png')
+Product.create(name: '"I\'ll Drink to That" Shirt', price: 19.95, img_path: 'logo-shirt.png')
+Product.create(name: '"I\'ll Drink to That" Koozie', price: 4.95, img_path: 'logo-koozie.png')
+
+puts "Seeded the db"
